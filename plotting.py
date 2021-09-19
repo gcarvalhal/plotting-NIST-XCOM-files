d0481ea9bd5384b3407b1934b728e7938f88286a
gcarvalhal <goncalosousatelescarvalhal@gmail.com>
2021-09-19 16:30:37 +0100

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -1,1 +1,17 @@
 
+# encoding: utf-8
+
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+filename = "XCOM Cu.txt"
+
+df = pd.read_csv(filename, delimiter=" ", comment="#")
+
+x = df.iloc[:, 0]
+y = df.iloc[:, 7]
+
+plt.plot(x, y, 'o', label=filename)
+plt.title(filename)
+plt.show()
